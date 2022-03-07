1c07a66922370cd4d9f9783ad751004935bd66b3
mrff88 <mrff88@gmail.com>
2022-03-06 22:42:05 -0500

    add task.controller.js file to controller directory

---
+++ api/controllers/task.controller.js
@@ -0,0 +1,53 @@
+import { Task } from '../models/index.js';
+
+//Controller to get all Tasks
+export const getAllTasks = async (request, response) => {
+  try {
+    const tasks = await Task.find();
+    if (tasks.length === 0) response.status(204).send();
+    else response.status(200).json(tasks);
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+};
+
+//Controller to Create a task
+export const createTask = async (request, response) => {
+  const task = new Task(request.body);
+  try {
+    const newTask = await task.save();
+    newTask && response.status(201).json(newTask);
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+};
+//Controller to update a task
+export const updateTask = async (request, response) => {
+  const taskToUpdate = request.body;
+  const { task } = request.data;
+  try {
+    Task.updateOne(task, taskToUpdate, (error, updatedTask) => {
+      !error
+        ? response.status(200).json(updatedTask)
+        : response.status(500).send(error);
+    });
+  } catch (error) {
+    response.status(500).send(error);
+  }
+};
+
+//Controller to delete a task
+export const deleteTask = async (response, request) => {
+  const { id: idTask } = request.params;
+  try {
+    const taskToDelete = await Task.findById(idTask);
+    if (!taskToDelete)
+      response.status(204).send({ error: 'No task to delete' });
+    else {
+      const deletedTask = await Task.deleteOne(taskToDelete);
+      if (deletedTask) response.status(200).json(deletedTask);
+    }
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+};
