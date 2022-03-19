bb02be5dec172df9c5c53498a46391477cf66552
mrff88 <mrff88@gmail.com>
2022-03-19 12:59:39 -0500

    add emailSender service

---
+++ api/services/emailSender.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+
+const emailSender = {
+  config: {},
+  transporter: function () {
+    const { host, port, secure, auth } = this.config;
+    const transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure,
+      auth,
+    });
+    return transporter;
+  },
+  sendMail: async function (data) {
+    const transporter = this.transporter();
+    let info = await transporter.sendMail(data);
+    return info;
+  },
+};
+
+export default emailSender;
